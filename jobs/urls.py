f97029f822fdf1ffba47e543df6c449aeda66c0f
shubhraneel <shubhraneelpal@gmail.com>
2021-04-03 14:19:26 +0530

    Add Job Posting

--- jobs/urls.py
+++ jobs/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 from .views import *
+
 urlpatterns = [
-    path('', get_jobs, name='jobs')
+    path('', get_jobs, name='jobs'),
+    path('post/', post_job, name='post_job'),
+    path('update/<str:pk>/', update_job, name='update_job'),
+    path('delete/<str:pk>/', delete_job, name='delete_job')
 ]
--- jobs/views.py
+++ jobs/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from accounts.decorators import company_required
-from .models import *
+from .models import JobPosting
 from .forms import JobPostingForm
 from accounts.models import Company
+from django.http import HttpResponse
 
 # Create your views here.
 
@@ -22,11 +23,38 @@ def post_job(request):
     form = JobPostingForm()
     if request.method == 'POST':
         form = JobPostingForm(request.POST)
-        # todo company linking
-        form.company = Company.objects.get(id=request.user)
+        form.company = Company.objects.get(id=request.user.id)
         if form.is_valid():
             form.save()
-            return redirect('announcements')
+            return redirect('jobs')
     context = {'form': form}
     return render(request, 'form.html', context=context)
+
+@login_required
+@company_required
+def update_job(request, pk):
+    job = JobPosting.objects.get(id=pk)
+    if request.user.id != job.company.user.id:
+        return HttpResponse("Unauthorized Access", 403)
+    form = JobPostingForm(instance=job)
+    if request.method == 'POST':
+        form = JobPostingForm(request.POST, instance=job)
+        if form.is_valid():
+            form.save()
+            return redirect('jobs')
+    context = {'form': form}
+    return render(request, 'form.html', context=context)
+
+
+@login_required
+@company_required
+def delete_job(request, pk):
+    job = JobPosting.objects.get(id=pk)
+    if request.user.id != job.company.user.id:
+        return HttpResponse("Unauthorized Access", 403)
+    if request.method == 'POST':
+        job.delete()
+        return redirect('jobs')
+    context = {'item': job}
+    return render(request, 'delete.html', context=context)
 
